edd795ed9678fa00d5820498afc6a076a5f03a87
David Nikolaj Vinje <davv@itu.dk>
2022-06-30 12:33:04 +0200

    day3

--- Day3.py
+++ Day3.py
@@ -1,47 +1,48 @@
-def part1():
-    f = open("day3.txt")
-    x, y = 0, 0
-    pos = set()
-    pos.add((x, y))
-    line = f.readline()
-    for char in line:
-        if char == "<":
-            x -= 1
-        elif char == ">":
-            x += 1
-        elif char == "^":
-            y += 1
-        else:
-            y -= 1
-        pos.add((x, y))
-    print(len(pos))
-
-def part2():
-    f = open("day3.txt")
-    line = f.readline()
-    x1, y1, x2, y2 = 0, 0, 0, 0
-    pos = set()
-    pos.add((x1,y1))
-    for idx in range(0, len(line), 2):
-        if line[idx] == "<":
-            x1 -= 1
-        elif line[idx] == ">":
-            x1 += 1
-        elif line[idx] == "^":
-            y1 += 1
-        else: 
-            y1 -= 1
-        pos.add((x1,y1))    
-        
-        if line[idx+1] == "<":
-            x2 -= 1
-        elif line[idx+1] == ">":
-            x2 += 1
-        elif line[idx+1] == "^":
-            y2 += 1
-        else: 
-            y2 -= 1
-        pos.add((x2,y2))
-    print(len(pos))
-part2()
+def part_one():
+  f = open('day3.txt', 'r')
+  x, y = 0, 0
+  houses = {(0,0)}
+  for i in list(f.readline()):
+    if i == '>':
+      x = x+1
+    elif i == '<':
+      x = x-1
+    elif i == '^':
+      y = y-1
+    else:
+      y = y+1
+    houses.add((x,y))
+  print(len(houses))
     
+def part_two():
+  f = open('day3.txt', 'r')
+  x1, y1, x2, y2 = 0, 0, 0, 0
+  houses = {(0,0)}
+  turn = 0
+  for i in list(f.readline()):
+    if turn % 2 == 0:
+      if i == '>':
+        x1 = x1+1
+      elif i == '<':
+        x1 = x1-1
+      elif i == '^':
+        y1 = y1-1
+      else:
+        y1 = y1+1
+      houses.add((x1,y1))
+    else:
+      if i == '>':
+        x2 = x2+1
+      elif i == '<':
+        x2 = x2-1
+      elif i == '^':
+        y2 = y2-1
+      else:
+        y2 = y2+1
+      houses.add((x2,y2))
+    turn = turn+1
+      
+  print(len(houses))
+  
+# part_one()
+part_two()
--- day.py
+++ day.py
@@ -1,17 +1,48 @@
 def part_one():
-  paper = 0
-  for line in open('day2.txt', 'r'):
-    l, w, h = [int(i) for i in line.split('x')]
-    paper = paper + 2*l*w + 2*w*h + 2*h*l + min(l*w,w*h,h*l)
-  print(paper)
+  f = open('day3.txt', 'r')
+  x, y = 0, 0
+  houses = {(0,0)}
+  for i in list(f.readline()):
+    if i == '>':
+      x = x+1
+    elif i == '<':
+      x = x-1
+    elif i == '^':
+      y = y-1
+    else:
+      y = y+1
+    houses.add((x,y))
+  print(len(houses))
     
 def part_two():
-  ribbon = 0
-  for line in open('day2.txt', 'r'):
-    l = [int(i) for i in line.split('x')]
-    ls = sorted(l)[:-1]
-    ribbon = ribbon + ls[0]*2 + ls[1]*2 + l[0]*l[1]*l[2]
-  print(ribbon)
+  f = open('day3.txt', 'r')
+  x1, y1, x2, y2 = 0, 0, 0, 0
+  houses = {(0,0)}
+  turn = 0
+  for i in list(f.readline()):
+    if turn % 2 == 0:
+      if i == '>':
+        x1 = x1+1
+      elif i == '<':
+        x1 = x1-1
+      elif i == '^':
+        y1 = y1-1
+      else:
+        y1 = y1+1
+      houses.add((x1,y1))
+    else:
+      if i == '>':
+        x2 = x2+1
+      elif i == '<':
+        x2 = x2-1
+      elif i == '^':
+        y2 = y2-1
+      else:
+        y2 = y2+1
+      houses.add((x2,y2))
+    turn = turn+1
+      
+  print(len(houses))
   
 # part_one()
 part_two()
